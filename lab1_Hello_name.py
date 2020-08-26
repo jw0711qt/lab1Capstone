5243c4369d74d84e7a4cdad7a76cee13b9dd810a
micha <jw0711qt@go.minnstate.edu>
2020-08-26 00:30:27 -0500

    fila

---
+++ lab1_Hello_name.py
@@ -0,0 +1,6 @@
+name=input("what is your name?")
+birthdate=input("what month were you born?")
+
+print("Hello" + name)
+print("Happy birthday"+ birthdate)
+print("There are"+ str(len(name))+"letters in your name")
